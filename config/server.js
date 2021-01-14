0e1dee738d4eba74f089e15c5e8e9c51d4c3bf83
elbader17 <badereduardo@gmail.com>
2021-01-14 17:13:23 -0300

    commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2ddb3c0105ddeedd09852197a0d20db3'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3db8ec79-4136-4837-a1fe-f03788c9e7a2'
+};
